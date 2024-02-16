a36dc38bbfba7d7bb77a9ab1286017a67bc8e11b
Shubham Kumar <shubhammuz110203@gmail.com>
2024-02-16 21:20:12 +0530

    API connected

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,21 +1,51 @@
+import { useEffect, useState, useCallback } from 'react'
+import toast from "react-hot-toast";
 import Search from '../components/Search'
 import SortRepos from '../components/SortRepos'
 import ProfileInfo from '../components/ProfileInfo'
 import Repos from '../components/Repos'
 import Spinner from '../components/Spinner'
-import { useState } from 'react'
 
 const HomePage = () => {
-  const [userprofile, setuserProfile] = useState(null);
-  
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("forks");
+
+  const getUserProfileAndRepos = useCallback(async() => {
+      setLoading(true);
+      try {
+        const userRes = await fetch('https://api.github.com/users/shubham-kumar-iem');
+        const userProfile = await userRes.json();
+        setUserProfile(userProfile);
+
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        setRepos(repos);
+        console.log("User Profile:", userProfile);
+        console.log("User repos", repos);
+      } 
+      catch (error) {
+        toast.error(error.message);
+      }
+      finally {
+        setLoading(false);
+      }
+    }, []);
+
+    useEffect(() => {
+      getUserProfileAndRepos();
+    }, [getUserProfileAndRepos]);
+
   return (
     <div className='m-4'>
       <Search/>
       <SortRepos/>
       <div className='flex gap-4 flex-col lg:flex-row justify-center items-center'>
-        <ProfileInfo/>
-        <Repos/>
-        <Spinner/>
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile}/>}
+        {repos.length > 0 && !loading && <Repos repos={repos}/>}
+        
+        {loading && <Spinner/>}
       </div>
     </div>
   )
