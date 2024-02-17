99aae201f33c50f45bc7ec1aab83bed6c282f3e0
Shubham Kumar <shubhammuz110203@gmail.com>
2024-02-17 23:28:15 +0530

    api routes setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback } from "react";
 import toast from "react-hot-toast";
-import Search from '../components/Search'
-import SortRepos from '../components/SortRepos'
-import ProfileInfo from '../components/ProfileInfo'
-import Repos from '../components/Repos'
-import Spinner from '../components/Spinner'
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Spinner from "../components/Spinner";
 
 const HomePage = () => {
   const [userProfile, setUserProfile] = useState(null);
@@ -12,81 +12,71 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
   const [sortType, setSortType] = useState("recent");
 
-  const getUserProfileAndRepos = useCallback(async(username="Shubham-kumar-iem") => {
+  const getUserProfileAndRepos = useCallback(
+    async (username = "Shubham-kumar-iem") => {
       setLoading(true);
       try {
-        const userRes = await fetch(`https://api.github.com/users/${username}`, {
-          headers:{
-            authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-          },
-        });
+        const res = await fetch(`http://localhost:5000/api/users/profile/${username}`);
+        const {repos, userProfile} = await res.json();
 
-        const userProfile = await userRes.json();
-        setUserProfile(userProfile);
-
-        const repoRes = await fetch(userProfile.repos_url);
-        const repos = await repoRes.json();
-        // console.log("User Profile:", userProfile);
-        // console.log("User repos", repos);
-        repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
-        
+        repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
         setRepos(repos);
-        return {userProfile, repos};
+        setUserProfile(userProfile);
+        return { userProfile, repos };
       } 
       catch (error) {
         toast.error(error.message);
-      }
+      } 
       finally {
         setLoading(false);
       }
-    }, []);
+    },
+    []
+  );
 
-    useEffect(() => {
-      getUserProfileAndRepos();
-    }, [getUserProfileAndRepos]);
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
 
-    const onSearch = async (e, username) => {
-      e.preventDefault();
-  
-      setLoading(true);
-      setRepos([]);
-      setUserProfile(null);
-  
-      const { userProfile, repos } = await getUserProfileAndRepos(username);
-  
-      setUserProfile(userProfile);
-      setRepos(repos);
-      setLoading(false);
-      setSortType("recent");
-    };
-    const onSort = (sortType) => {
-      if(sortType == "recent") {
-        repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
-      }
-      else if(sortType == "stars") {
-        repos.sort((a,b) => b.stargazers_count - a.stargazers_count);
-      }
-      else if(sortType = "forks") {
-        repos.sort((a,b) => b.forks_count - a.forks_count);
-      }
-      setSortType(sortType);
-      setRepos([...repos]);
-    };
-  
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+    setSortType("recent");
+  };
+  const onSort = (sortType) => {
+    if (sortType == "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (sortType == "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    } else if ((sortType = "forks")) {
+      repos.sort((a, b) => b.forks_count - a.forks_count);
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
 
   return (
-    <div className='m-4'>
-      <Search onSearch={onSearch}/>
-      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType}/>}
+    <div className="m-4">
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       {/* <SortRepos/> */}
-      <div className='flex gap-4 flex-col lg:flex-row justify-center items-center'>
-        {userProfile && !loading && <ProfileInfo userProfile={userProfile}/>}
-        {!loading && <Repos repos={repos}/>}
-        
-        {loading && <Spinner/>}
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-center">
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {!loading && <Repos repos={repos} />}
+
+        {loading && <Spinner />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
